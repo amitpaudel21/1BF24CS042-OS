7f34cbbb091a713652c24ab54ea26e09ec9153c9
Amit Paudel <amitpaudel.cs24@bmsce.ac.in>
2026-05-29 13:45:28 +0530

    Add files via upload

---
+++ PRIORITY/priority(preemtive).c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+struct Process
+{
+    int pid, at, bt, rt, pr;
+    int ct, tat, wt;
+};
+
+int main()
+{
+    int n;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct Process p[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].pid = i + 1;
+
+        printf("\nProcess %d\n", i + 1);
+
+        printf("Arrival Time: ");
+        scanf("%d", &p[i].at);
+
+        printf("Burst Time: ");
+        scanf("%d", &p[i].bt);
+
+        printf("Priority: ");
+        scanf("%d", &p[i].pr);
+
+        p[i].rt = p[i].bt;
+    }
+
+    int complete = 0;
+    int time = 0;
+
+    while (complete < n)
+    {
+        int idx = -1;
+        int highestPriority = 9999;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].at <= time &&
+                p[i].rt > 0 &&
+                p[i].pr < highestPriority)
+            {
+                highestPriority = p[i].pr;
+                idx = i;
+            }
+        }
+
+        if (idx == -1)
+        {
+            time++;
+            continue;
+        }
+
+        p[idx].rt--;
+        time++;
+
+        if (p[idx].rt == 0)
+        {
+            complete++;
+
+            p[idx].ct = time;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+        }
+    }
+
+    float avgWT = 0, avgTAT = 0;
+
+    printf("\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid,
+               p[i].at,
+               p[i].bt,
+               p[i].pr,
+               p[i].ct,
+               p[i].tat,
+               p[i].wt);
+
+        avgWT += p[i].wt;
+        avgTAT += p[i].tat;
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avgWT / n);
+    printf("\nAverage Turnaround Time = %.2f\n", avgTAT / n);
+
+    return 0;
+}
